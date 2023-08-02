6100d51778c27fe98206b4886b0b395472f52384
“Romoking <cloudeliteabroad@gmail.com>
2023-08-02 12:24:39 -0400

    m

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('good wishes!'));
+app.get('/', (req, res) => res.send('good wishes lovelies!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
